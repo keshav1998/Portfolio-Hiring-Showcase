a5e1be0cfdc1c930338cc26148f1b9ff43fbe386
Keshav Mishra <keshav.mishra0298@gmail.com>
2023-07-25 03:25:59 +0530

    edit: personal info

--- app/page.tsx
+++ app/page.tsx
@@ -35,26 +35,17 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Keshav Mishra, I'm a Senion ML and MLOps engineer at{" "}
 					<Link
 						target="_blank"
 						href="https://upstash.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						Mechademy
 					</Link>
 
 					<br />
-					and working on{" "}
-					<Link
-						target="_blank"
-						href="https://unkey.dev"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						unkey.dev
-					</Link>{" "}
-					at night.
+					and in free time I build my project using opensource solutions at night.
 				</h2>
 			</div>
 		</div>
